542b361049e67c37e2f38cba9cfa7c2935cff2a8
sheltonkwiterovich <skwiterovich@aol.com>
2021-03-02 21:31:33 -0500

    pseudo code

---
+++ black_jack.py
@@ -0,0 +1,11 @@
+
+# make a deck, aces can be 1 or 11
+# dealer: deal player two cards and themselves two cards, only showing one 
+# player must get hit or pass 
+# goal: get as close to 21 as possible 
+# if player goes over 21 they bust and the dealer wins 
+# otherwise 
+# the dealer draws until they either have a higher hand then the player or they bust
+# if the dealer busts the player wins
+
+# OPTIONAL *** add betting system
